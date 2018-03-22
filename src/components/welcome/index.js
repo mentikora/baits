0241d4f335cfdc1378f7763676a963d0ae8c7cde
Roman Horobets <roman.horobets@rolique.io>
2018-03-22 18:38:40 +0200

    welcome section title

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -4,8 +4,18 @@ import './styles.css';
 class Welcome extends Component {
   render() {
     return (
-      <section className="board">
-        test board section
+      <section className="welcome">
+        <div className="content">
+          <h1 className="welcome__title">
+            cu
+            <span className="welcome__title--red">s</span>
+            tom
+            <br />
+            bai
+            <span className="welcome__title--yellow">t</span>
+            s
+          </h1>
+        </div>
       </section>
     );
   }
