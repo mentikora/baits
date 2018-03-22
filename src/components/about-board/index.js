92d473c041e239fac76ae68706cd90990c58ce3b
Roman Horobets <roman.horobets@rolique.io>
2018-03-22 14:58:37 +0200

    update

---
+++ src/components/about-board/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class AboutBoard extends Component {
+  render() {
+    return (
+      <section className="about">
+        test about section
+      </section>
+    );
+  }
+}
+
+export default AboutBoard;
---
+++ src/components/welcome-board/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class WelcomeBoard extends Component {
+  render() {
+    return (
+      <section className="board">
+        test board section
+      </section>
+    );
+  }
+}
+
+export default WelcomeBoard;
