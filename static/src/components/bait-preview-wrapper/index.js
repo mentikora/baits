ee1cbc7b9de1c4420f1f44c50053a51d6f0fc04a
Roman Horobets <roman.horobets@rolique.io>
2018-03-26 19:12:52 +0300

    bait item preview view

--- static/src/components/bait-preview-wrapper/index.js
+++ static/src/components/bait-preview-wrapper/index.js
@@ -40,13 +40,48 @@ class BaitPreviewWrapper extends Component {
   			weight: '2g',
   			price: '65'
   		},
-  		{/*{
-  			id: '',
-  			image: '',
-  			name: '',
-  			weight: '',
-  			price: '65'
-  		}*/}
+      {
+        id: '7',
+        image: 'https://lh3.googleusercontent.com/q4DjuVAxe71wr82r1BMgYwtRv20irV3L9SRRW4PZhExsfc5utfD--zo4k1U7BIcZ2o3xVhBI_7Ks80vFfJNMQJ0yRagYknnX-XYPWhbg1xjXUTO-gNqtD5oOQg4yy77ehsQ3Yt_I',
+        name: 'SONIC',
+        weight: '1.5g',
+        price: '65'
+      },
+      {
+        id: '8',
+        image: 'https://lh3.googleusercontent.com/q4DjuVAxe71wr82r1BMgYwtRv20irV3L9SRRW4PZhExsfc5utfD--zo4k1U7BIcZ2o3xVhBI_7Ks80vFfJNMQJ0yRagYknnX-XYPWhbg1xjXUTO-gNqtD5oOQg4yy77ehsQ3Yt_I',
+        name: 'CANDY',
+        weight: '1.6g',
+        price: '65'
+      },
+      {
+        id: '9',
+        image: 'https://lh3.googleusercontent.com/q4DjuVAxe71wr82r1BMgYwtRv20irV3L9SRRW4PZhExsfc5utfD--zo4k1U7BIcZ2o3xVhBI_7Ks80vFfJNMQJ0yRagYknnX-XYPWhbg1xjXUTO-gNqtD5oOQg4yy77ehsQ3Yt_I',
+        name: 'MATILDA',
+        weight: '2.2g',
+        price: '65'
+      },
+      {
+        id: '10',
+        image: 'https://lh3.googleusercontent.com/q4DjuVAxe71wr82r1BMgYwtRv20irV3L9SRRW4PZhExsfc5utfD--zo4k1U7BIcZ2o3xVhBI_7Ks80vFfJNMQJ0yRagYknnX-XYPWhbg1xjXUTO-gNqtD5oOQg4yy77ehsQ3Yt_I',
+        name: 'CHILI',
+        weight: '2.7g',
+        price: '65'
+      },
+      {
+        id: '11',
+        image: 'https://lh3.googleusercontent.com/q4DjuVAxe71wr82r1BMgYwtRv20irV3L9SRRW4PZhExsfc5utfD--zo4k1U7BIcZ2o3xVhBI_7Ks80vFfJNMQJ0yRagYknnX-XYPWhbg1xjXUTO-gNqtD5oOQg4yy77ehsQ3Yt_I',
+        name: 'OZZY',
+        weight: '2.2g',
+        price: '65'
+      },
+      {
+        id: '12',
+        image: 'https://lh3.googleusercontent.com/q4DjuVAxe71wr82r1BMgYwtRv20irV3L9SRRW4PZhExsfc5utfD--zo4k1U7BIcZ2o3xVhBI_7Ks80vFfJNMQJ0yRagYknnX-XYPWhbg1xjXUTO-gNqtD5oOQg4yy77ehsQ3Yt_I',
+        name: 'NEMO',
+        weight: '1.2g',
+        price: '65'
+      }
 		];
   	return (
 			<div className="content">
--- static/src/components/bait-preview/index.js
+++ static/src/components/bait-preview/index.js
@@ -6,20 +6,26 @@ class BaitPreview extends Component {
   	let item = this.props;
   	return (
   		<div className="bait-preview">
-  			<div className="bait-preview__image">
-  				<img src={item.image} alt={item.name} title={item.name} />
-  			</div>
-  			<h1 className="bait-preview__name heading-2">
-  				{item.name}
-  			</h1>
-  			<div className="bait-preview__weight">
-  				{item.weight}
-  			</div>
-  			<div className="bait-preview__price">
-  				{item.price}
-  			</div>
+        <a href="javascript:void(0);" className="bait-preview__link">
+    			<div className="bait-preview__image">
+    				<img src={item.image} alt={item.name} title={item.name} />
+    			</div>
+          <div className="bait-preview__content">
+            <h1 className="bait-preview__title">
+              <span className="bait-preview__name heading-2">
+                {item.name}
+              </span>
+              <span className="bait-preview__weight">
+                {item.weight}
+              </span>
+            </h1>
+      			<div className="bait-preview__price">
+      				{item.price} $
+      			</div>
+          </div>
+        </a>
   		</div>
-		); 
+		);
   }
 }
 
