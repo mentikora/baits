0789bfee7288f7a6647d9d3880f53fdfcff2ca53
Roman Horobets <roman.horobets@rolique.io>
2018-04-24 18:00:27 +0300

    baits colors

--- static/src/components/bait-colors/index.js
+++ static/src/components/bait-colors/index.js
@@ -5,11 +5,26 @@ const baitColorsData = [
   {
     id: 'color1',
     name: 'col. 001',
-    image: 'https://static.pexels.com/photos/192454/pexels-photo-192454.jpeg',
+    image: 'https://static.pexels.com/photos/294674/pexels-photo-294674.jpeg',
   },
   {
     id: 'color2',
     name: 'col. 002',
+    image: 'https://static.pexels.com/photos/294674/pexels-photo-294674.jpeg',
+  },
+  {
+    id: 'color3',
+    name: 'col. 003',
+    image: 'https://static.pexels.com/photos/294674/pexels-photo-294674.jpeg',
+  },
+  {
+    id: 'color4',
+    name: 'col. 004',
+    image: 'https://static.pexels.com/photos/294674/pexels-photo-294674.jpeg',
+  },
+  {
+    id: 'color5',
+    name: 'col. 005',
     image: 'https://static.pexels.com/photos/294674/pexels-photo-294674.jpeg',
   },
 ];
@@ -20,14 +35,16 @@ class BaitColors extends Component {
       <div className="content">
         <div className="bait-colors-wrapper">
           {
-            baitColorsData.map( (el, key) =>  <div className="bait-color" key={el.name}>
-              <div className="bait-color__image-wrapper">
-                <img scr={el.image} alt={el.name} title={el.name} />
+            baitColorsData.map((el, key) => (
+              <div className="bait-color" key={el.name}>
+                <div className="bait-color__image-wrapper">
+                  <img src={el.image} alt={el.name} title={el.name} />
+                </div>
+                <h2 className="bait-color__name heading-2">
+                  {el.name}
+                </h2>
               </div>
-              <h2 className="bait-color__name heading-2">
-                {el.name}
-              </h2>
-            </div>)
+            ))
           }
         </div>
       </div>
